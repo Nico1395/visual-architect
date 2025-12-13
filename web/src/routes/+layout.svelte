7d47bfc30033995049ec144386ad263c5977df55
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2025-12-13 23:28:25 +0100

    Switched to sveltekit

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<main>
+	<header>
+
+	</header>
+
+	<div>
+		{@render children()}
+	</div>
+
+	<footer>
+
+	</footer>
+</main>
+
+<style lang="scss">
+	$test: #234223;
+</style>
