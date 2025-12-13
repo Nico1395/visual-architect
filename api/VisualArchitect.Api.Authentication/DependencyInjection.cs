2c2667277f04b3306d6c82e23b535221b860f947
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2025-12-13 17:08:29 +0100

    Added a solution and first projects with CPM in an api directory

---
+++ api/VisualArchitect.Api.Authentication/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VisualArchitect.Api.Authentication;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddVisualArchitectAuthentication(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ api/VisualArchitect.Api.Orchestration/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VisualArchitect.Api.Orchestration;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddVisualArchitectOrchestration(this IServiceCollection services)
+    {
+        return services;
+    }
+}
