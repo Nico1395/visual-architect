6e1507a82bfdcdda5466f76e5339f42bd0dd6918
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2025-12-13 18:04:00 +0100

    Created the vite svelte template with routing

---
+++ web/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Router from 'svelte-spa-router';
+  import Counter from './lib/Counter.svelte'
+  import NotFound from './lib/pages/NotFound.svelte';
+
+  const routes = {
+    '/': Counter,
+    '*': NotFound,
+  };
+</script>
+
+<Router {routes} />
+
+<style>
+</style>
---
+++ web/src/lib/pages/NotFound.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    console.log("not found");
+</script>
+
+<h1>
+    Not Found
+</h1>
+
+<style>
+</style>
