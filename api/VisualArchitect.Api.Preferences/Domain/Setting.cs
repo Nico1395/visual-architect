1c570496072dd73a64aedba49807c0e4f1396d39
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2025-12-20 23:32:19 +0100

    Moved settings to a new 'preferences' lib

--- api/VisualArchitect.Api.Preferences/Domain/Setting.cs
+++ api/VisualArchitect.Api.Preferences/Domain/Setting.cs
@@ -4,5 +4,5 @@ internal sealed class Setting
 {
     public int Id { get; init; }
     public required string Key { get; init; }
-    public required string DefaultValue { get; init; }
+    public string? DefaultValue { get; init; }
 }
