55bb6fd40ae2528a563e926f398fdc7a23b3aefa
Nico1395 <119791859+Nico1395@users.noreply.github.com>
2025-12-27 00:22:45 +0100

    Application design domain adjustments

--- api/VisualArchitect.Api.ApplicationDesign/Domain/Design.cs
+++ api/VisualArchitect.Api.ApplicationDesign/Domain/Design.cs
@@ -5,10 +5,9 @@ namespace VisualArchitect.Api.ApplicationDesign.Domain;
 public sealed class Design : ICreated, IUpdated
 {
     public Guid Id { get; init; } = Guid.NewGuid();
-    public required Guid TaskApplicationId { get; init; }
-    public required long TaskNumber { get; init; }
+    public required Guid TaskId { get; init; }
     public required string Name { get; set; }
-    public string? CommentPayload { get; set; }
+    public string? DescriptionPayload { get; set; }
     public DesignType Type { get; set; }
     public required string Payload { get; set; }
     public DateTime CreatedAt { get; set; }
--- api/VisualArchitect.Api.ApplicationDesign/Domain/DesignTask.cs
+++ api/VisualArchitect.Api.ApplicationDesign/Domain/DesignTask.cs
@@ -5,10 +5,10 @@ namespace VisualArchitect.Api.ApplicationDesign.Domain;
 public sealed class DesignTask : ICreated, IUpdated
 {
     public Guid Id { get; init; } = Guid.NewGuid();
-    public required Guid ApplicationId { get; init; }
+    public required Guid ProjectId { get; init; }
     public required long Number { get; init; }
     public required string Name { get; set; }
-    public required string Payload { get; set; }
+    public required string DescriptionPayload { get; set; }
     public DesignTaskStatus Status { get; set; }
     public List<Design>? Designs { get; set; }
     public DateTime CreatedAt { get; set; }
